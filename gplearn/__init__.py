026200140004c7ccf187dedf09263660d069de29
summershaaa <wenjx1024@qq.com>
2019-06-14 21:49:58 +0800

    Add files via upload

---
+++ gplearn/__init__.py
@@ -0,0 +1,8 @@
+"""Genetic Programming in Python, with a scikit-learn inspired API
+
+``gplearn`` is a set of algorithms for learning genetic programming models.
+
+"""
+__version__ = '0.3.0'
+
+__all__ = ['genetic', 'functions', 'fitness']
---
+++ gplearn/functions.py
@@ -0,0 +1,173 @@
+"""The functions used to create programs.
+
+The :mod:`gplearn.functions` module contains all of the functions used by
+gplearn programs. It also contains helper methods for a user to define their
+own custom functions.
+"""
+
+# Author: Trevor Stephens <trevorstephens.com>
+#
+# License: BSD 3 clause
+
+import numpy as np
+from sklearn.externals import six
+
+__all__ = ['make_function']
+
+
+class _Function(object):
+
+    """A representation of a mathematical relationship, a node in a program.
+
+    This object is able to be called with NumPy vectorized arguments and return
+    a resulting vector based on a mathematical relationship.
+
+    Parameters
+    ----------
+    function : callable
+        A function with signature function(x1, *args) that returns a Numpy
+        array of the same shape as its arguments.
+
+    name : str
+        The name for the function as it should be represented in the program
+        and its visualizations.
+
+    arity : int
+        The number of arguments that the ``function`` takes.
+
+    """
+
+    def __init__(self, function, name, arity):
+        self.function = function
+        self.name = name
+        self.arity = arity
+
+    def __call__(self, *args):
+        return self.function(*args)
+
+
+def make_function(function, name, arity):
+    """Make a function node, a representation of a mathematical relationship.
+
+    This factory function creates a function node, one of the core nodes in any
+    program. The resulting object is able to be called with NumPy vectorized
+    arguments and return a resulting vector based on a mathematical
+    relationship.
+
+    Parameters
+    ----------
+    function : callable
+        A function with signature `function(x1, *args)` that returns a Numpy
+        array of the same shape as its arguments.
+
+    name : str
+        The name for the function as it should be represented in the program
+        and its visualizations.
+
+    arity : int
+        The number of arguments that the `function` takes.
+
+    """
+    if not isinstance(arity, int):
+        raise ValueError('arity must be an int, got %s' % type(arity))
+    if not isinstance(function, np.ufunc):
+        if six.get_function_code(function).co_argcount != arity:
+            raise ValueError('arity %d does not match required number of '
+                             'function arguments of %d.'
+                             % (arity,
+                                six.get_function_code(function).co_argcount))
+    if not isinstance(name, six.string_types):
+        raise ValueError('name must be a string, got %s' % type(name))
+
+    # Check output shape
+    args = [np.ones(10) for _ in range(arity)]
+    try:
+        function(*args)
+    except ValueError:
+        raise ValueError('supplied function %s does not support arity of %d.'
+                         % (name, arity))
+    if not hasattr(function(*args), 'shape'):
+        raise ValueError('supplied function %s does not return a numpy array.'
+                         % name)
+    if function(*args).shape != (10,):
+        raise ValueError('supplied function %s does not return same shape as '
+                         'input vectors.' % name)
+
+    # Check closure for zero & negative input arguments
+    args = [np.zeros(10) for _ in range(arity)]
+    if not np.all(np.isfinite(function(*args))):
+        raise ValueError('supplied function %s does not have closure against '
+                         'zeros in argument vectors.' % name)
+    args = [-1 * np.ones(10) for _ in range(arity)]
+    if not np.all(np.isfinite(function(*args))):
+        raise ValueError('supplied function %s does not have closure against '
+                         'negatives in argument vectors.' % name)
+
+    return _Function(function, name, arity)
+
+
+def _protected_division(x1, x2):
+    """Closure of division (x1/x2) for zero denominator."""
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.where(np.abs(x2) > 0.001, np.divide(x1, x2), 1.)
+
+def _protected_arccosh(x1):
+    """Closure of division (x1/x2) for zero denominator."""
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.where(x1 >= 1, np.arccosh(x1), 0)
+
+def _protected_sqrt(x1):
+    """Closure of square root for negative arguments."""
+    return np.sqrt(np.abs(x1))
+
+
+def _protected_log(x1):
+    """Closure of log for zero arguments."""
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.where(np.abs(x1) > 0.001, np.log(np.abs(x1)), 0.)
+
+
+def _protected_inverse(x1):
+    """Closure of log for zero arguments."""
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.where(np.abs(x1) > 0.001, 1. / x1, 0.)
+
+add2 = make_function(function=np.add, name='add', arity=2)
+sub2 = make_function(function=np.subtract, name='sub', arity=2)
+mul2 = make_function(function=np.multiply, name='mul', arity=2)
+div2 = make_function(function=_protected_division, name='div', arity=2)
+sqrt1 = make_function(function=_protected_sqrt, name='sqrt', arity=1)
+log1 = make_function(function=_protected_log, name='log', arity=1)
+neg1 = make_function(function=np.negative, name='neg', arity=1)
+inv1 = make_function(function=_protected_inverse, name='inv', arity=1)
+abs1 = make_function(function=np.abs, name='abs', arity=1)
+max2 = make_function(function=np.maximum, name='max', arity=2)
+min2 = make_function(function=np.minimum, name='min', arity=2)
+sin1 = make_function(function=np.sin, name='sin', arity=1)
+cos1 = make_function(function=np.cos, name='cos', arity=1)
+tan1 = make_function(function=np.tan, name='tan', arity=1)
+exp1 = make_function(function=np.exp, name='exp', arity=1)
+arcsinh1 = make_function(function=np.arcsinh, name='arcsinh', arity=1)
+arccosh1 = make_function(function=_protected_arccosh, name='arccosh', arity=1)
+arcsin1 = make_function(function=np.arcsin, name='arcsin', arity=1)
+arccos1 = make_function(function=np.arccos, name='arccos', arity=1)
+
+_function_map = {'add': add2,
+                 'sub': sub2,
+                 'mul': mul2,
+                 'div': div2,
+                 'sqrt': sqrt1,
+                 'log': log1,
+                 'abs': abs1,
+                 'neg': neg1,
+                 'inv': inv1,
+                 'max': max2,
+                 'min': min2,
+                 'sin': sin1,
+                 'cos': cos1,
+                 'tan': tan1 ,
+                 'exp':exp1,
+                 'arcsinh':arcsinh1,
+                 'arccosh':arccosh1,
+                 'arcsin':arcsin1,
+                 'arccos':arccos1,}
